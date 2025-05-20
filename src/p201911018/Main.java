6b021f991a4987429cb43bcf007e298cabc7244e
Shin, Hyunwoo <woh52123@gmail.com>
2025-05-21 03:07:08 +0900

    one

---
+++ src/p201911018/Main.java
@@ -0,0 +1,8 @@
+package p201911018;
+
+public class Main {
+	  public static void main(String[] args) {
+	    // 자신의 학번과 이름을 출력하라
+	    System.out.println("201514099 홍길동");  // 여기에 본인의 학번과 이름 입력
+	  }
+	}

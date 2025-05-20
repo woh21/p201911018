5c7749b7e251af442eddb7f0162eba6f0715acaa
Shin, Hyunwoo <woh52123@gmail.com>
2025-05-21 03:15:26 +0900

    two

---
+++ src/p201911018/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+  public static int sum(int n) {
+    int result = 0;
+    for (int i = 1; i <= n; ++i)
+      result += i;
+    return result;
+  }
+}
--- src/p201911018/Main.java
+++ src/p201911018/Main.java
@@ -1,8 +1,11 @@
 package p201911018;
 
 public class Main {
-	  public static void main(String[] args) {
-	    // 자신의 학번과 이름을 출력하라
-	    System.out.println("201514099 홍길동");  // 여기에 본인의 학번과 이름 입력
-	  }
-	}
+  public static void main(String[] args) {
+    // 자신의 학번과 이름을 입력하라
+    System.out.println("201911018 홍길동");
+    
+    System.out.println(Hello.sum(10));
+    System.out.println(Hello.sum(100));
+  }
+}

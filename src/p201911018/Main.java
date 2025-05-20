cbec7236469e14b68be1750d0f803c7ccb272e3f
Shin, Hyunwoo <woh52123@gmail.com>
2025-05-21 03:28:56 +0900

    five

--- src/p201911018/Main.java
+++ src/p201911018/Main.java
@@ -1,12 +1,15 @@
 package p201911018;
 
 public class Main {
-	  public static void main(String[] args) {
-	    // 자신의 학번과 이름을 입력하라
-	    System.out.println("201911018 홍길동");
-	    
-	    System.out.println(Hello.sum(10));
-	    System.out.println(Hello.sum(100));
-	    System.out.println(Hello.sum(1000));
-	  }
-	}
+  public static void main(String[] args) {
+    // 자신의 학번과 이름을 입력하라
+    System.out.println("201911018 홍길동");
+    
+    System.out.println(Hello.sum(10));
+    System.out.println(Hello.sum(100));
+    System.out.println(Hello.sum(1000));
+    
+    System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+    System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
+  }
+}

30d0bc7576894fa6e1ff9abb21e42469a10782c9
Shin, Hyunwoo <woh52123@gmail.com>
2025-05-21 03:20:55 +0900

    three

--- src/p201911018/Hello.java
+++ src/p201911018/Hello.java
@@ -1,8 +1,5 @@
 public class Hello {
   public static int sum(int n) {
-    int result = 0;
-    for (int i = 1; i <= n; ++i)
-      result += i;
-    return result;
+    return (n == 1) ? 1 : n + sum(n - 1);
   }
 }
--- src/p201911018/Main.java
+++ src/p201911018/Main.java
@@ -1,11 +1,12 @@
 package p201911018;
 
 public class Main {
-  public static void main(String[] args) {
-    // 자신의 학번과 이름을 입력하라
-    System.out.println("201911018 홍길동");
-    
-    System.out.println(Hello.sum(10));
-    System.out.println(Hello.sum(100));
-  }
-}
+	  public static void main(String[] args) {
+	    // 자신의 학번과 이름을 입력하라
+	    System.out.println("201911018 홍길동");
+	    
+	    System.out.println(Hello.sum(10));
+	    System.out.println(Hello.sum(100));
+	    System.out.println(Hello.sum(1000));
+	  }
+	}
